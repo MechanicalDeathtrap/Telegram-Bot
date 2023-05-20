b51db7117eadd2d2569757bcafd90eecedfaa239
MechanicalDeathtrap <korikova-04@mail.ru>
2023-05-20 16:07:40 +0300

    Bot.py

--- main.py
+++ main.py
@@ -4,115 +4,104 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot('5979986889:AAHbxQv1xLf0QncYBQUh25Z4NTeAXNjOHPQ')
-# не нравится
-userName = None
-userGroup = None
-userTg = None
+
 
 @bot.message_handler(commands=["start"])
-def start(m, res=False):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Помощь")
-    btn2 = types.KeyboardButton("Зарегистрироваться")
-    markup.add(btn1)
-    markup.add(btn2)
-    bot.send_message(m.chat.id, 'Добро пожаловать в телеграм бот ITIS Request', reply_markup=markup)
-    bot.send_message(m.chat.id, 'Здесь публикуются все объявления о наборе помощников для организаторов '
-                                '(культорг, спорторг, профорг, инфорг, соцорг, научорг, организатор волонтёров)')
-    bot.send_message(m.chat.id, 'Немножечко о балловой системе '
-                                '\nОБЩАЯ ИНФОРМАЦИЯ: '
-                                '\n— Каждый семестр студенты могут получить дополнительные баллы за участие '
-                                'в мероприятиях и событиях студенческой жизни. '
-                                '\nДополнительные баллы являются частью общего рейтинга на заселение у иногородних '
-                                'студентов. Помимо этого, их можно добавить к академическим баллам по некоторым предметам. '
-                                '\n— Все активности оцениваются в соответствии с регламентом дополнительных баллов:'
-                                ' https://docs.google.com/document/d/1QTSgofEYUjkWy6ALJ. '
-                                '\n— Свои текущие баллы можно посмотреть здесь:'
-                                ' https://docs.google.com/spreadsheets/d/1qPCCiMbWs1EZa.')
+def start(message):
+    start_message = "Добро пожаловать в Itis-Requset Bot!\n"
+    start_message += "Чтобы посмотреть доступные команды и узнать больше о боте нажми /help\n"
+    bot.send_message(message.chat.id,start_message)
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(m):
-    if m.text == 'Помощь':
-        bot.send_message(m.chat.id, 'Если вам нужна помощь, пожалуйста, напишите организаторам мероприятия:')
-        bot.send_message(m.chat.id, 'Сергей Бабич : Председатель Студсовета - https://vk.com/kreomanser')
-        bot.send_message(m.chat.id, 'Александра : Кузнецова Профорг - https://vk.com/alekscscs')
-        bot.send_message(m.chat.id, 'Аня Будревич : Культорг - https://vk.com/ann_utochkka')
-        bot.send_message(m.chat.id, 'Кирилл Ефремов : Спорторг - https://vk.com/kirillefremov45')
-        bot.send_message(m.chat.id, 'Елизавета Мухортова : Руководитель Медиацентра - https://vk.com/lr_107')
-        bot.send_message(m.chat.id, 'Валентин Грачёв : Научорг - https://vk.com/valentin_grachyov')
-        bot.send_message(m.chat.id, 'Rumiya Salakhova :  Организатор Волонтеров - https://vk.com/angel277')
-        bot.send_message(m.chat.id, 'Дмитрий Сметанин : Руководитель Отдела Безопасности - https://vk.com/dmitriysmetanin')
-        bot.send_message(m.chat.id, 'Андрей Осин : Руководитель Антикоррупционного направления - https://vk.com/h8m3_3')
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    help_message = "Добро пожаловать в помощь!\n\n"
+    help_message += "Доступные команды:\n"
+    help_message += "/help - Отобразить доступные команды и контакты организаторов, немного о баллах\n"
+    help_message += "/show - Показать доступные мероприятия\n"
+    help_message += "/new - Создать новое событие\n"
+    help_message += "/register - Зарегистрироваться (нужно для записи на мероприятие)\n\n"
+    help_message += "Общая информация:\n"
+    help_message += "— Каждый семестр студенты могут получить дополнительные \n"
+    help_message += "баллы за участие в мероприятиях и событиях студенческой жизни.\n"
+    help_message += "— Дополнительные баллы являются частью общего рейтинга на заселение у иногородних студентов. Помимо этого, их можно \n"
+    help_message += "добавить к академическим баллам по некоторым предметам.\n"
+    help_message += "— Все активности оцениваются в соответствии с регламентом дополнительных баллов:\n"
+    help_message += "https://docs.google.com/document/d/1QTSgofEYUjkWy6ALJ.\n\n"
+    help_message += "Если вам нужна помощь, пожалуйста, напишите организаторам мероприятия:\n"
+    help_message += "Сергей Бабич : Председатель Студсовета - https://vk.com/kreomanser\n"
+    help_message += "Александра Кузнецова : Профорг - https://vk.com/alekscscs\n"
+    help_message += "Аня Будревич : Культорг - https://vk.com/ann_utochkka\n"
+    help_message += "Кирилл Ефремов : Спорторг - https://vk.com/kirillefremov45\n"
+    help_message += "Елизавета Мухортова : Руководитель Медиацентра - https://vk.com/lr_107\n"
+    help_message += "Валентин Грачёв : Научорг - https://vk.com/valentin_grachyov\n"
+    help_message += "Rumiya Salakhova :  Организатор Волонтеров - https://vk.com/angel277\n"
+    help_message += "Дмитрий Сметанин : Руководитель Отдела Безопасности - https://vk.com/dmitriysmetanin\n"
+    help_message += "Андрей Осин : Руководитель Антикоррупционного направления - https://vk.com/h8m3_3\n"
 
-    if m.text == 'Зарегистрироваться':
-        # создание файла бдшки
-        connection = sqlite3.connect('Users.sql')
-
-        # через курсор выполняем команды бдшки
-        cursor = connection.cursor()
-
-        # подготавливаем команду для создания таблицы бд с полями group name telegLink если она ещё не существует
-        cursor.execute('CREATE TABLE IF NOT EXISTS users '
-                       '(id int auto_increment primary key,'
-                       ' name varchar(60),'
-                       ' uGroup varchar(6),'
-                       ' tLink varchar(20))')
-
-        # создаём таблицу и добавляем ее в файл с бд
-        connection.commit()
-
-        # закрывем соединение
-        cursor.close()
-        connection.close()
-
-        bot.send_message(m.chat.id, 'Привет! Для регистрации введи своё ФИО ')
-        bot.register_next_step_handler(m, user_name)
+    bot.send_message(message.chat.id, help_message)
 
 
-def user_name(m):
-    global userName
-    userName = m.text.strip()
-    bot.send_message(m.chat.id, 'Ещё номер группы ) ')
-    bot.register_next_step_handler(m, user_group)
+@bot.message_handler(commands=['register'])
+def register_user(message):
+    conn = sqlite3.connect('BotUsers.db')
+    cursor = conn.cursor()
 
-def user_group(m):
-    global userGroup
-    userGroup = m.text.strip()
-    bot.send_message(m.chat.id, 'Ну и напоследок твой tg ')
-    bot.register_next_step_handler(m, user_tg)
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER,
+            full_name TEXT,
+            group_number TEXT,
+            telegram_link TEXT,
+            is_admin INTEGER
+        )
+    ''')
+    conn.commit()
 
-def user_tg(m):
-    global userTg
-    userTg = m.text.strip()
+    user_id = message.from_user.id
+    cursor.execute('SELECT * FROM users WHERE user_id=?', (user_id,))
+    existing_user = cursor.fetchone()
 
-    connection = sqlite3.connect('Users.sql')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO users (name , uGroup , tLink) '
-                   'VALUES ("%s" , "%s", "%s" )' % (userName, userGroup, userTg))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    if existing_user:
+        bot.reply_to(message, "Вы уже зарегистрировались!")
+    else:
+        bot.reply_to(message, "Введите своё ФИО:")
+        bot.register_next_step_handler(message, process_full_name, user_id)
 
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardMarkup("Users", callback_data = 'list'))
 
-    bot.send_message(m.chat.id, f" Поздравляю ,{userName}, вы зарегистрированы!)", reply_markup= markup )
+def process_full_name(message, chat_id):
+    full_name = message.text.strip()
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    connection = sqlite3.connect('Users.sql')
-    cursor = connection.cursor()
+    bot.send_message(chat_id, "Введите номер группы:")
+    bot.register_next_step_handler(message, process_group_number, chat_id, full_name)
 
-    cursor.execute('SELECT * FROM users')
-    users = cursor.fetchall()
 
-    info =''
-    for item in users:
-        info+= f"Name :{item[1]}, group:{item[2]}, tg:{item[3]}\n"
+def process_group_number(message, chat_id, full_name):
+    group_number = message.text.strip()
 
-    cursor.close()
-    connection.close()
-    bot.send_message(call.m.chat.id, info)
+    bot.send_message(chat_id, "Введите свой tg")
+    bot.register_next_step_handler(message, process_telegram_link, chat_id, full_name, group_number)
+
+
+def process_telegram_link(message, chat_id, full_name, group_number):
+
+    conn = sqlite3.connect('BotUsers.db')
+    cursor = conn.cursor()
+
+    telegram_link = message.text.strip()
+    user_id = message.from_user.id
+    is_admin = 1 if user_id in [123456789] else 0  # Нужно заменить на ID админов
+
+    cursor.execute('INSERT INTO users (user_id, full_name, group_number, telegram_link, is_admin) '
+                   'VALUES ( ?, ?, ?, ?, ?)',
+                   (user_id, full_name, group_number, telegram_link, is_admin))
+    conn.commit()
+
+    
+    bot.reply_to(message, f"Регистрация завершена! Добро пожаловать, {full_name}!")
+
+    if is_admin:
+        bot.send_message(chat_id, "Вам предоставлены права администратора!")
+
 
 bot.polling(none_stop=True, interval=0)
